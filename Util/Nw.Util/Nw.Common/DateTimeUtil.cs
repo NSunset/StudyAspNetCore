943ffa0d2f10b4712cb007ddfe6e5bd787f094c5
nw <524873829@qq.com>
2022-07-06 21:43:25 +0800

    时间类型相关操作统一放到DateTimeUtil里面了

--- Util/Nw.Util/Nw.Common/DateTimeUtil.cs
+++ Util/Nw.Util/Nw.Common/DateTimeUtil.cs
@@ -6,16 +6,27 @@ using System.Threading.Tasks;
 
 namespace Nw.Common
 {
-    public class DateTimeUtil
+    public static class DateTimeUtil
     {
         private static readonly string[] day = new string[] { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+
+        /// <summary>
+        /// 计算当天所在周是一年中的第几周
+        /// </summary>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        public static int WeekOfYear(int day)
+        {
+            System.Globalization.GregorianCalendar gc = new System.Globalization.GregorianCalendar();
+            return gc.GetWeekOfYear(DateTime.Now, System.Globalization.CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+        }
 
         /// <summary>
         /// 获取指定时间当前周的开始时间
         /// </summary>
         /// <param name="time">指定时间</param>
         /// <returns></returns>
-        public DateTime GetCurrentWeekStart(DateTime time)
+        public static DateTime GetCurrentWeekStart(DateTime time)
         {
             DateTime startWeek = time.AddDays(1 - Convert.ToInt32(time.DayOfWeek.ToString("d")));
             return startWeek;
@@ -26,7 +37,7 @@ namespace Nw.Common
         /// </summary>
         /// <param name="time">指定时间</param>
         /// <returns></returns>
-        public DateTime GetCurrentWeekEnd(DateTime time)
+        public static DateTime GetCurrentWeekEnd(DateTime time)
         {
             return GetCurrentWeekStart(time).AddDays(6);
         }
@@ -36,7 +47,7 @@ namespace Nw.Common
         /// </summary>
         /// <param name="time">指定时间</param>
         /// <returns></returns>
-        public DateTime GetCurreentMonthStart(DateTime time)
+        public static DateTime GetCurreentMonthStart(DateTime time)
         {
             return time.AddDays(1 - time.Day);
         }
@@ -46,7 +57,7 @@ namespace Nw.Common
         /// </summary>
         /// <param name="time">指定时间</param>
         /// <returns></returns>
-        public DateTime GetCurreentMonthEnd(DateTime time)
+        public static DateTime GetCurreentMonthEnd(DateTime time)
         {
             return GetCurreentMonthStart(time).AddMonths(1).AddDays(-1);
         }
@@ -56,7 +67,7 @@ namespace Nw.Common
         /// </summary>
         /// <param name="time">指定时间</param>
         /// <returns></returns>
-        public DateTime GetCurrentQuarterStart(DateTime time)
+        public static DateTime GetCurrentQuarterStart(DateTime time)
         {
             return time.AddMonths(0 - (time.Month - 1) % 3).AddDays(1 - time.Day);
         }
@@ -66,7 +77,7 @@ namespace Nw.Common
         /// </summary>
         /// <param name="time">指定时间</param>
         /// <returns></returns>
-        public DateTime GetCurrentQuarterEnd(DateTime time)
+        public static DateTime GetCurrentQuarterEnd(DateTime time)
         {
             return GetCurrentQuarterStart(time).AddMonths(3).AddDays(-1);
         }
@@ -76,7 +87,7 @@ namespace Nw.Common
         /// </summary>
         /// <param name="time">指定时间</param>
         /// <returns></returns>
-        public DateTime GetCurrentYearStart(DateTime time)
+        public static DateTime GetCurrentYearStart(DateTime time)
         {
             return new DateTime(time.Year, 1, 1);
         }
@@ -86,7 +97,7 @@ namespace Nw.Common
         /// </summary>
         /// <param name="time">指定时间</param>
         /// <returns></returns>
-        public DateTime GetCurrentYearEnd(DateTime time)
+        public static DateTime GetCurrentYearEnd(DateTime time)
         {
             return new DateTime(time.Year, 12, 31); ;
         }
@@ -96,10 +107,222 @@ namespace Nw.Common
         /// </summary>
         /// <param name="time"></param>
         /// <returns></returns>
-        public string GetChineseWeek(DateTime time)
+        public static string GetChineseWeek(DateTime time)
         {
             string week = day[Convert.ToInt32(time.DayOfWeek.ToString("d"))].ToString();
             return week;
+        }
+
+        /// <summary>
+        /// 可为空的时间格式转为DateTime?
+        /// </summary>
+        /// <param name="time">可为空的时间格式</param>
+        /// <param name="defaulTime">
+        /// 转换失败给的时间；默认为null
+        /// </param>
+        /// <returns></returns>
+        public static DateTime? ToDateTime(this object time, DateTime? defaulTime = null)
+        {
+            if (!Equals(time, null))
+            {
+                DateTime result;
+                if (DateTime.TryParse(time.ToString(), out result))
+                {
+                    return result;
+                }
+            }
+            return defaulTime;
+        }
+
+        /// <summary>
+        /// 时间格式字符串转为DateTime
+        /// </summary>
+        /// <param name="time">时间格式的字符串</param>
+        /// <param name="defaulTime">
+        /// 转换失败给的时间；默认为DateTime.MinValue
+        /// </param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this string time, DateTime? defaulTime = null)
+        {
+            DateTime result;
+            if (defaulTime == null)
+            {
+                defaulTime = DateTime.MinValue;
+            }
+            if (DateTime.TryParse(time, out result))
+            {
+                return result;
+            }
+            return defaulTime.Value;
+        }
+
+        /// <summary>
+        /// 格式化字符串，带时分秒格式 "yyyy-MM-dd HH:mm:ss"
+        /// </summary>
+        /// <param name="time">要格式化的时间</param>
+        /// <param name="removeSecond">是否删除秒</param>
+        /// <returns></returns>
+        public static string ToDateTimeString(this DateTime time, bool removeSecond = false)
+        {
+            if (removeSecond)
+                return time.ToString("yyyy-MM-dd HH:mm");
+            return time.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        /// <summary>
+        /// 格式化字符串，带时分秒格式 "yyyy-MM-dd HH:mm:ss"
+        /// </summary>
+        /// <param name="time">要格式化的时间</param>
+        /// <param name="removeSecond">是否删除秒</param>
+        /// <returns></returns>
+        public static string ToDateTimeString(this DateTime? time, bool removeSecond = false)
+        {
+            if (time == null)
+                return string.Empty;
+            return time.Value.ToDateTimeString(removeSecond);
+        }
+
+        /// <summary>
+        /// 格式化字符串，格式 "yyyy-MM-dd"
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ToDateString(this DateTime time)
+        {
+            return time.ToString("yyyy-MM-dd");
+        }
+
+        /// <summary>
+        /// 格式化字符串，格式 "yyyy-MM-dd"
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ToDateString(this DateTime? time)
+        {
+            if (time == null)
+                return string.Empty;
+            return time.Value.ToDateString();
+        }
+
+        /// <summary>
+        /// 格式化字符串，格式 "HH:mm:ss"
+        /// </summary>
+        /// <param name="time">要格式化的时间</param>
+        /// <param name="removeSecond">是否删除秒</param>
+        /// <returns></returns>
+        public static string ToTimeString(this DateTime time, bool removeSecond = false)
+        {
+            if (removeSecond)
+                return time.ToString("HH:mm");
+            return time.ToString("HH:mm:ss");
+        }
+
+        /// <summary>
+        /// 格式化字符串， 格式"HH:mm:ss"
+        /// </summary>
+        /// <param name="time">要格式化的时间</param>
+        /// <param name="removeSecond">是否删除秒</param>
+        /// <returns></returns>
+        public static string ToTimeString(this DateTime? time, bool removeSecond = false)
+        {
+            if (time == null)
+                return string.Empty;
+            return time.Value.ToTimeString(removeSecond);
+        }
+
+        /// <summary>
+        /// 格式化字符串带毫秒，格式 "yyyy-MM-dd HH:mm:ss.fff"
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ToMillisecondString(this DateTime time)
+        {
+            return time.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        }
+
+        /// <summary>
+        /// 格式化字符串带毫秒，格式 "yyyy-MM-dd HH:mm:ss.fff"
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ToMillisecondString(this DateTime? time)
+        {
+            if (time == null)
+                return string.Empty;
+            return time.Value.ToMillisecondString();
+        }
+
+        /// <summary>
+        /// 获取格式化字符串，不带时分秒，格式："yyyy年MM月dd日"
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ToChineseDateString(this DateTime time)
+        {
+            return string.Format("{0}年{1}月{2}日", time.Year, time.Month.ToString("00"), time.Day.ToString("00"));
+        }
+
+        /// <summary>
+        /// 获取格式化字符串，不带时分秒，格式："yyyy年MM月dd日"
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ToChineseDateString(this DateTime? time)
+        {
+            if (time == null)
+                return string.Empty;
+            return time.Value.ToChineseDateString();
+        }
+
+        /// <summary>
+        /// 获取格式化字符串，带时分秒，格式："yyyy年MM月dd日 HH时mm分ss秒"
+        /// </summary>
+        /// <param name="dateTime">日期</param>
+        /// <param name="removeSecond">是否移除秒</param>
+        public static string ToChineseDateTimeString(this DateTime dateTime, bool removeSecond = false)
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendFormat(dateTime.ToChineseDateString());
+            result.AppendFormat(" {0}时{1}分", dateTime.Hour.ToString("00"), dateTime.Minute.ToString("00"));
+            if (removeSecond == false)
+                result.AppendFormat("{0}秒", dateTime.Second.ToString("00"));
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// 获取格式化字符串，带时分秒，格式："yyyy年MM月dd日 HH时mm分ss秒"
+        /// </summary>
+        /// <param name="dateTime">日期</param>
+        /// <param name="removeSecond">是否移除秒</param>
+        /// <returns></returns>
+        public static string ToChineseDateTimeString(this DateTime? dateTime, bool removeSecond = false)
+        {
+            if (dateTime == null)
+                return string.Empty;
+            return dateTime.Value.ToChineseDateTimeString(removeSecond);
+        }
+
+        /// <summary>
+        /// 获取描述
+        /// </summary>
+        /// <param name="span">时间间隔</param>
+        /// <returns></returns>
+        public static string ToDescription(this TimeSpan span)
+        {
+            StringBuilder result = new StringBuilder();
+            if (span.Days > 0)
+                result.AppendFormat("{0}天", span.Days);
+            if (span.Hours > 0)
+                result.AppendFormat("{0}小时", span.Hours);
+            if (span.Minutes > 0)
+                result.AppendFormat("{0}分", span.Minutes);
+            if (span.Seconds > 0)
+                result.AppendFormat("{0}秒", span.Seconds);
+            if (span.Milliseconds > 0)
+                result.AppendFormat("{0}毫秒", span.Milliseconds);
+            if (result.Length > 0)
+                return result.ToString();
+            return string.Format("{0}毫秒", span.TotalSeconds * 1000);
         }
     }
 }
